6890fdfb4015249bed8640128d08cf507babf3e0
FedeAngeriz <10086@holbertonstudents.com>
2024-12-21 20:39:11 +0000

    Update task shell

--- my_shell.c
+++ my_shell.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
+#define MAX_ARGS 128
+
 int main()
 {
 	char *comando = NULL;
 	size_t largo = 0;
-	int args = 0;
-	char *token = strtok(comando, " ");
+	char *args[MAX_ARGS];
+	char *token;
+	int contador = 0;
 
 	printf("Bienvenido a la Shcaloneta\n");
 
@@ -20,15 +23,23 @@ int main()
 			break;
 		}
 
+		comando[strcspn(comando, "\n")] = '\0';
+
 		if (strcmp(comando, "exit") == 0)
 		{
 			printf("Se va La Shcaloneta...\n");
 			break;
 		}
-	}
-	while (token != NULL)
-	{
-		token = strtok(NULL, " ");
+
+		token = strtok(comando, " ");
+		
+		while (token != NULL && contador < MAX_ARGS - 1)
+		{
+			args[contador++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[contador] = NULL;
+
 	}
 	free(comando);
 	return (0);
